0de3678d6980b36eb719953379562bea0adf295d
eldeni <eldeniyenden@gmail.com>
2018-10-01 17:37:39 +0900

    refactor: create common function that allows for generating server
    - createServer() generates a barebone express static server. With enhance(),
    callers can customize the express application based on the relevant bundles
    and all.
    - LaunchStatus is defined as constant.
    
    Fixes https://github.com/eldeni/react-boilerplate/issues/20

--- src/client/config/appConfig.js
+++ src/client/config/appConfig.js
@@ -5,6 +5,10 @@ const appConfig = {
     graph: false,
     navigation: false,
   },
+  ports: {
+    local: 5001,
+    prod: 5001,
+  },
   reduxStateKey: '__PRELOAED_STATE__',
 };
 
--- src/server/server.local.js
+++ src/server/server.local.js
@@ -1,88 +1,43 @@
 import express from "express";
 import fs from 'fs';
 import path from "path";
-import { Provider as ReduxProvider } from 'react-redux';
-import React from "react";
-import { renderToString } from "react-dom/server";
-import { StaticRouter } from 'react-router-dom';
 import webpack from 'webpack';
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
 
-import appConfig from '@config/appConfig';
-import configureStore from '../client/state/configureStore';
-import makeHtml from './makeHtml';
-import RootContainer from '@containers/app/RootContainer/RootContainer.web';
+import { calculateNextStateWhileSearchingForBundles } from './serverUtils';
+import createServer from './createServer';
+import LaunchStatus from './constants/LaunchStatus';
 import webpackConfig from '../../internals/webpack/webpack.config.client.local.web';
 
-console.log(123, webpackConfig);
+export default createServer({
+  enhance: (app, state) => {
+    console.info('server local - webpack configuration', webpackConfig);
+    const webpackCompiler = webpack(webpackConfig);
 
-const webpackCompiler = webpack(webpackConfig);
+    app.use(webpackDevMiddleware(webpackCompiler, {
+      publicPath: webpackConfig.output.publicPath,
+      serverSideRender: true,
+      stats: {
+        color: true,
+      },
+    }));
 
-const PORT = 5001;
+    app.use(webpackHotMiddleware(webpackCompiler, {
+      heartbeat: 2000,
+    }));
 
-const SERVER_STATUS = {
-  LAUNCH_ERROR: 'LAUNCH_ERROR',
-  LAUNCH_SUCCESS: 'LAUNCH_SUCCESS',
-  NOT_LAUNCHED: 'NOT_LAUNCHED',
-};
-
-const app = express();
-const state = {
-  entrypointBundles: [],
-  status: SERVER_STATUS.NOT_LAUNCHED,
-};
-
-app.use(htmlLogger);
-
-app.use(webpackDevMiddleware(webpackCompiler, {
-  publicPath: webpackConfig.output.publicPath,
-  stats: {
-    color: true,
+    app.use((req, res, next) => {
+      if (state.LaunchStatus !== LaunchStatus.NOT_LAUNCHED) {
+        const info = res.locals.webpackStats.toJson({
+          all: false,
+          assets: true,
+          builtAt: true,
+          entrypoints: true,
+        });
+        state.update(calculateNextStateWhileSearchingForBundles(info.entrypoints));
+      }
+      next();
+    });
   },
-}));
-
-app.use(webpackHotMiddleware(webpackCompiler, {
-  heartbeat: 2000,
-}));
-
-app.get("/*", (req, res) => {
-  console.log(1)
-  res.send({
-    power: 1,
-  })
-  // const store = configureStore();
-
-  // if (state.status !== SERVER_STATUS.LAUNCH_SUCCESS) {
-  //   res.writeHead(500);
-  //   res.end('server is not launched');
-  // } else {
-  //   const element = (
-  //     <ReduxProvider store={store}>
-  //       <StaticRouter 
-  //         context={{}}
-  //         location={req.url}>
-  //         <RootContainer/>
-  //       </StaticRouter>
-  //     </ReduxProvider>
-  //   );
-  //   const elementInString = renderToString(element);
-
-  //   res.writeHead(200, { "Content-Type": "text/html" });
-  //   res.end(makeHtml({
-  //     reactDom: elementInString,
-  //     reduxState: store,
-  //     reduxStateKey: appConfig.reduxStateKey,
-  //     bundles: state.entrypointBundles,
-  //   }));
-  // }
 });
-
-app.listen(PORT, () => {
-  console.info('Server listening: %s', PORT);
-});
-
-function htmlLogger(req, res, next) {
-  console.info('%s f- url: %s, user agent: %s', new Date(), req.url, req.get('User-Agent'));
-  next();
-}
